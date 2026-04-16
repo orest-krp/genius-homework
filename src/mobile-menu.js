6c5073652e2c931d7aaf3e604974a6ce514eb5f6
orest-krp <albor1veras@gmail.com>
2026-04-16 17:59:37 +0200

    improving header and intro

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -3,10 +3,10 @@ const SCREEN_MD = 768;
 
 class MobileMenu {
     constructor() {
-        this.burger = document.querySelector('.header__burger');
-        this.overlay = document.querySelector('.header__mobile-overlay');
-        this.mobileMenu = document.querySelector('.header__mobile');
-        this.logo = document.querySelector('.header__logo');
+        this.burger = document.getElementById('burger-button');
+        this.overlay = document.getElementById('mobile-overlay');
+        this.mobileMenu = document.getElementById('mobile-menu-container');
+        this.logo = document.getElementById('main-logo');
         this.isOpen = false;
 
         this.init();
@@ -47,16 +47,20 @@ class MobileMenu {
 
     openMenu() {
         this.burger.classList.add('header__burger--active');
+        this.burger.setAttribute('aria-expanded', 'true');
         this.overlay.classList.add('header__mobile-overlay--active');
         this.mobileMenu.classList.add('header__mobile--active');
+        this.mobileMenu.removeAttribute('inert');
         document.body.classList.add('body--no-scroll');
         this.isOpen = true;
     }
 
     closeMenu() {
         this.burger.classList.remove('header__burger--active');
+        this.burger.setAttribute('aria-expanded', 'false');
         this.overlay.classList.remove('header__mobile-overlay--active');
         this.mobileMenu.classList.remove('header__mobile--active');
+        this.mobileMenu.setAttribute('inert', true);
         document.body.classList.remove('body--no-scroll');
         this.isOpen = false;
     }
